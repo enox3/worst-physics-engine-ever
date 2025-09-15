c592c07669c0fb40fb8a79518d202e0270fefa44
enox3 <Thorben.Rahns@gmail.com>
2025-09-15 09:07:33 +0200

    Clippy fix

--- src/won.rs
+++ src/won.rs
@@ -35,8 +35,7 @@ fn setup(
 ) {
     progression.levels[level.0] = LEVELS[level.0]
         .thresholds
-        .binary_search(&colliders.coords.len())
-        .map_or_else(|err| err, |ok| ok)
+        .binary_search(&colliders.coords.len()).unwrap_or_else(|err| err)
         .min(progression.levels[level.0]);
 
     // Common style for all buttons on the screen
